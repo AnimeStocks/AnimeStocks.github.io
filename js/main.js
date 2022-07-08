31a9d3124ce50d6f6dcbbffccfdfd97639953453
Bermar Villarazo Jr <88958976+BermarVillarazo@users.noreply.github.com>
2022-07-08 14:06:18 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,22 @@ if (close) {
         navbar.classList.remove('active');
     })
 }
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++) {
+        var windowheigth = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 90;
+
+        if(revealtop < windowheigth - revealpoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
