3b79fd41aa41c70ba120ef9e93b04883267a54ea
Bermar Villarazo Jr <88958976+BermarVillarazo@users.noreply.github.com>
2022-07-10 13:04:43 +0800

    Add files via upload

---
+++ js/TAC.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('header');
+let prevScroll = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+    let currentScrollUp = window.pageYOffset;
+
+    if (prevScroll < currentScrollUp) {
+        nav.classList.add('hide');
+    }
+    else {
+        nav.classList.remove('hide');
+    }
+    prevScroll = currentScrollUp;
+})
